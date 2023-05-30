9cd97e302c69486b735c313afbcbb9ae9a125727
StephFallesen <stephanie1507@live.dk>
2023-05-30 14:43:41 +0200

    Rettelser

---
+++ javascript/login.js
@@ -0,0 +1,21 @@
+function login() {
+  // Henter email og adgangskode fra inputfelterne
+  var usernameInput = document.querySelector('input[name="username"]');
+  var passwordInput = document.querySelector('input[name="password"]');
+  var username = usernameInput.value;
+  var password = passwordInput.value;
+
+  // Simuleret login med persona
+  if (username === "malene@gmail.com" && password === "Malene2109") {
+    console.log("Log ind gennemført!");
+    // Omdiriger til forsiden
+    window.location.href = "index.html";
+  } else {
+    console.log("Log ind fejlet. Forkert E-mail eller adgangskode.");
+    // Vis fejlbesked eller udfør anden håndtering af fejlet login
+  }
+
+  // Nulstil inputfelterne
+  usernameInput.value = "";
+  passwordInput.value = "";
+}
