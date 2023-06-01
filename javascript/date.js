cf8a9d5b00bc6bc4fbe7984fbacbec9a5d74de27
Anne-Sofie <95890841+Fidolicious@users.noreply.github.com>
2023-06-01 15:11:10 +0200

    age

--- javascript/date.js
+++ javascript/date.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // skaber reference til drop til dropdown
   const yearSelect = document.getElementById("year");
   const monthSelect = document.getElementById("month");
   const daySelect = document.getElementById("day");
-  const errorElement = document.getElementById("error");
-  
+  const errorMessage = document.getElementById("errorMessage");
   const months = ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December'];
 
+  // Månederne er altid det samme
   (function populateMonths() {
     for (let i = 0; i < months.length; i++) {
       const option = document.createElement('option');
@@ -18,11 +19,14 @@ document.addEventListener("DOMContentLoaded", function() {
   let previousDay;
 
   function populateDays(month) {
+    // sletter alle "children" fra dage og dropdown hvis disse eksistere 
     while (daySelect.firstChild) {
       daySelect.removeChild(daySelect.firstChild);
     }
-    
+
+    // Antallet af dage pr måned
     let dayNum;
+    // Hent nuværende år
     let year = yearSelect.value;
 
     if (month === 'Januar' || month === 'Marts' || month === 'Maj' || month === 'Juli' || month === 'August' || month === 'Oktober' || month === 'December') {
@@ -30,100 +34,89 @@ document.addEventListener("DOMContentLoaded", function() {
     } else if (month === 'April' || month === 'Juni' || month === 'September' || month === 'November') {
       dayNum = 30;
     } else {
+      // Check for skudår
       if (new Date(year, 1, 29).getMonth() === 1) {
         dayNum = 29;
       } else {
         dayNum = 28;
       }
     }
-
+    // indsætter korrekte dage i day feltet
     for (let i = 1; i <= dayNum; i++) {
       const option = document.createElement("option");
       option.textContent = i;
       daySelect.appendChild(option);
     }
-    
+
+    // Sæt den valgte dag imens værdien bevares hvis den eksistere
     if (previousDay) {
-      daySelect.value = previousDay;
-      
-      if (daySelect.value === "") {
-        daySelect.value = previousDay - 1;
-      }
-      if (daySelect.value === "") {
-        daySelect.value = previousDay - 2;
-      }
-      if (daySelect.value === "") {
-        daySelect.value = previousDay - 3;
+      if (previousDay <= dayNum) {
+        daySelect.value = previousDay;
+      } else {
+        daySelect.value = "";
       }
     }
   }
 
   function populateYears() {
+    // Hent det nuværende år som tal
     let year = new Date().getFullYear();
 
+    // Lad de forrige 100 år være en mulighed, så alle aldre er mulige
     for (let i = 0; i < 101; i++) {
       const option = document.createElement("option");
       option.textContent = year - i;
       yearSelect.appendChild(option);
     }
-    yearSelect.value = year;
+
+    yearSelect.value = year; // Sæt grundværdien for året
+
+    // Skriv fejlmeldingen når året
+    yearSelect.onchange = function() {
+      if (yearSelect.value !== yearSelect.firstElementChild.value) {
+        populateDays(monthSelect.value);
+        checkAge();
+      }
+    };
+  }
+
+  function checkAge() {
+    const selectedYear = parseInt(yearSelect.value);
+    const selectedMonth = monthSelect.value;
+    const selectedDay = parseInt(daySelect.value);
+
+    // Hent nuværende dato
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentDay = currentDate.getDate();
+
+    // Bregn alder
+    let age = currentYear - selectedYear;
+
+    // Check om fødselsdagen har været i år
+    if (currentMonth < months.indexOf(selectedMonth) + 1 || (currentMonth === months.indexOf(selectedMonth) + 1 && currentDay < selectedDay)) {
+      age--;
+    }
+
+    // Hvis fejlmelding hvis personens alder er under 16 og de har valgt et nyt år
+    if (selectedYear !== parseInt(yearSelect.firstElementChild.value) && age < 16) {
+      errorMessage.textContent = "Du skal mindst være 16 år for at være medlem af kundeklubben.";
+    } else {
+      errorMessage.textContent = ""; // Fjern fejlmelding
+    }
   }
 
   populateDays(monthSelect.value);
   populateYears();
 
-  yearSelect.onchange = function () {
-    const selectedYear = parseInt(yearSelect.value);
-    const selectedMonth = monthSelect.value;
-    const selectedDay = daySelect.value;
-
-    const currentDate = new Date();
-    const minimumDate = new Date(currentDate.getFullYear() - 18, currentDate.getMonth(), currentDate.getDate());
-
-    const selectedDate = new Date(selectedYear, months.indexOf(selectedMonth), selectedDay);
-
-    if (selectedDate > minimumDate) {
-      errorElement.textContent = "Du skal være mindst 18 år gammel.";
-    } else {
-      errorElement.textContent = "";
-    }
-
+  monthSelect.onchange = function() {
     populateDays(monthSelect.value);
+    checkAge();
   };
 
-  monthSelect.onchange = function () {
-    const selectedYear = parseInt(yearSelect.value);
-    const selectedMonth = monthSelect.value;
-    const selectedDay = daySelect.value;
-
-    const currentDate = new Date();
-    const minimumDate = new Date(currentDate.getFullYear() - 18, currentDate.getMonth(), currentDate.getDate());
-
-    const selectedDate = new Date(selectedYear, months.indexOf(selectedMonth), selectedDay);
-
-    if (selectedDate > minimumDate) {
-      errorElement.textContent = "Du skal være mindst 18 år gammel.";
-    } else {
-      errorElement.textContent = "";
-    }
-
-    populateDays(monthSelect.value);
-  };
-
-  daySelect.onchange = function () {
-    const selectedYear = parseInt(yearSelect.value);
-    const selectedMonth = monthSelect.value;
-    const selectedDay = daySelect.value;
-
-    const currentDate = new Date();
-    const minimumDate = new Date(currentDate.getFullYear() - 18, currentDate.getMonth(), currentDate.getDate());
-
-    const selectedDate = new Date(selectedYear, months.indexOf(selectedMonth), selectedDay);
-
-    if (selectedDate > minimumDate) {
-      errorElement.textContent = "Du skal være mindst 18 år gammel.";
-    } else {
-      errorElement.textContent = "";
-    }
+  daySelect.onchange = function() {
+    previousDay = parseInt(daySelect.value);
+    checkAge();
   };
 });
