e71b4a42470c826e1fda76fc4433ac7af371a56b
StephFallesen <stephanie1507@live.dk>
2023-06-03 22:24:56 +0200

    Styleguide

--- javascript/styleguide.js
+++ javascript/styleguide.js
@@ -16,3 +16,291 @@ function triggerExample2() {
   element.setSelectionRange(0, 99999);
   document.execCommand('copy');
 }
+function triggerExample3() {
+  const element = document.querySelector('#example3');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample4() {
+  const element = document.querySelector('#example4');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample5() {
+  const element = document.querySelector('#example5');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample6() {
+  const element = document.querySelector('#example6');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample7() {
+  const element = document.querySelector('#example7');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample8() {
+  const element = document.querySelector('#example8');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample9() {
+  const element = document.querySelector('#example9');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample10() {
+  const element = document.querySelector('#example10');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample11() {
+  const element = document.querySelector('#example11');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample12() {
+  const element = document.querySelector('#example12');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample13() {
+  const element = document.querySelector('#example13');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample14() {
+  const element = document.querySelector('#example14');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample15() {
+  const element = document.querySelector('#example15');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample16() {
+  const element = document.querySelector('#example16');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample17() {
+  const element = document.querySelector('#example17');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample18() {
+  const element = document.querySelector('#example18');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample19() {
+  const element = document.querySelector('#example19');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample20() {
+  const element = document.querySelector('#example20');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample21() {
+  const element = document.querySelector('#example21');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample22() {
+  const element = document.querySelector('#example22');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample23() {
+  const element = document.querySelector('#example23');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample24() {
+  const element = document.querySelector('#example40');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample25() {
+  const element = document.querySelector('#example25');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample26() {
+  const element = document.querySelector('#example26');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample27() {
+  const element = document.querySelector('#example27');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample28() {
+  const element = document.querySelector('#example28');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample29() {
+  const element = document.querySelector('#example29');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample30() {
+  const element = document.querySelector('#example30');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample31() {
+  const element = document.querySelector('#example31');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample32() {
+  const element = document.querySelector('#example32');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample33() {
+  const element = document.querySelector('#example33');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample34() {
+  const element = document.querySelector('#example34');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample35() {
+  const element = document.querySelector('#example35');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample36() {
+  const element = document.querySelector('#example36');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample37() {
+  const element = document.querySelector('#example37');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample38() {
+  const element = document.querySelector('#example38');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample39() {
+  const element = document.querySelector('#example39');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample40() {
+  const element = document.querySelector('#example40');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample41() {
+  const element = document.querySelector('#example41');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample42() {
+  const element = document.querySelector('#example42');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample43() {
+  const element = document.querySelector('#example43');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample44() {
+  const element = document.querySelector('#example44');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample45() {
+  const element = document.querySelector('#example45');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample46() {
+  const element = document.querySelector('#example46');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample47() {
+  const element = document.querySelector('#example47');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample48() {
+  const element = document.querySelector('#example48');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample49() {
+  const element = document.querySelector('#example49');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
+function triggerExample50() {
+  const element = document.querySelector('#example50');
+  element.select();
+  element.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+}
